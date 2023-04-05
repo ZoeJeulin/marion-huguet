4f400b5d2eaf370e578d4b84b675149f304ca71d
Zoé Jeulin <zoe.jeulin@gmail.com>
2023-04-05 19:31:51 +0200

    refactor header

---
+++ components/app/HeaderDesktop.vue
@@ -0,0 +1,113 @@
+<template>
+  <header class="header-desktop">
+    <ul class="nav-list">
+      <li v-for="(link, id) in global.navLinks" :key="id" class="nav-item">
+        <ui-icon name="etoile" class="star" />
+        <nuxt-link
+          :to="localePath({ name: link.slug ? link.slug : 'home' })"
+          class="t-cta-1"
+        >
+          {{ link.pageTitle }}
+        </nuxt-link>
+      </li>
+    </ul>
+    <div class="nav-item -right">
+      <ui-icon name="etoile" class="star" />
+      <a :href="global.donationsLink" target="_blank" class="t-cta-1">{{
+        global.donationsLabel
+      }}</a>
+    </div>
+  </header>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      global: (state) => state.global,
+    }),
+  },
+}
+</script>
+
+<style lang="scss">
+.header-desktop {
+  background: $blue1;
+  box-shadow: 0 20px 30px rgba($color: $blue2, $alpha: 0.3);
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid $beige;
+  padding: 30px 40px;
+  height: 90px;
+  position: fixed;
+  width: 100%;
+  z-index: 15;
+
+  @include below('sm') {
+    display: none;
+  }
+
+  .nav-item {
+    display: inline;
+    flex-direction: column;
+    position: relative;
+
+    &:hover {
+      &::before {
+        transform: scaleX(1);
+      }
+
+      .star {
+        opacity: 1;
+        transform: rotate(-45deg);
+      }
+    }
+
+    + .nav-item {
+      margin-left: 60px;
+    }
+
+    &::before {
+      content: '';
+      position: absolute;
+      bottom: -6px;
+      height: 1px;
+      background: $beige;
+      width: calc(100% - 5px);
+      z-index: 1;
+      transform: scaleX(0);
+      transform-origin: center left;
+      transition: transform 0.3s ease-out;
+
+      @include below('sm') {
+        width: 50px;
+      }
+    }
+
+    .star {
+      position: absolute;
+      left: 0;
+      right: 0;
+      margin: auto;
+      top: -25px;
+      width: 15px;
+      min-width: 15px;
+      opacity: 0;
+      transform: rotate(0deg);
+      transition: opacity 0.1s ease-out, transform 0.3s ease-out;
+    }
+
+    &.-right {
+      &::before {
+        width: 100%;
+      }
+
+      .star {
+        top: -15px;
+      }
+    }
+  }
+}
+</style>
---
+++ components/app/HeaderMobile.vue
@@ -0,0 +1,158 @@
+<template>
+  <header class="header-mobile">
+    <div class="header-nav">
+      <ui-icon class="typo" name="typo" />
+      <ui-burger-menu @click.native="toggleMenu" />
+    </div>
+    <div class="header-panel">
+      <ul class="nav-list">
+        <li v-for="(link, id) in global.navLinks" :key="id" class="nav-item">
+          <nuxt-link
+            :to="localePath({ name: link.slug ? link.slug : 'home' })"
+            class="t-cta-1"
+            @click.native="toggleMenu"
+          >
+            {{ link.pageTitle }}
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <a
+            :href="global.donationsLink"
+            target="_blank"
+            class="t-cta-1"
+            @click="toggleMenu"
+            >{{ global.donationsLabel }}</a
+          >
+        </li>
+      </ul>
+      <ui-icon class="whale" name="whale" />
+    </div>
+  </header>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      global: (state) => state.global,
+    }),
+  },
+  methods: {
+    toggleMenu() {
+      this.$el.querySelector('.ui-burger-menu').classList.toggle('-open')
+      this.$el.querySelector('.header-panel').classList.toggle('-show')
+      if (document.body.style.overflow === 'hidden') {
+        document.body.style.overflow = ''
+      } else if (document.body.style.overflow === '') {
+        document.body.style.overflow = 'hidden'
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.header-mobile {
+  display: none;
+  position: relative;
+  z-index: 5;
+
+  @include below('sm') {
+    display: block;
+  }
+
+  .header-nav {
+    position: fixed;
+    top: 0;
+    background: $blue1;
+    box-shadow: 0 20px 30px rgba($color: $blue2, $alpha: 0.3);
+    height: 60px;
+    padding: 10px 20px;
+    border-bottom: 1px solid $beige;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+
+    .typo {
+      width: 30%;
+    }
+
+    .ui-burger-menu {
+      &.-open {
+        svg {
+          .line-top {
+            transform: translateX(2px) rotate(44deg);
+            transition: transform 0.3s ease-out;
+          }
+          .line-middle {
+            opacity: 0;
+            transition: opacity 0.2s ease-out;
+          }
+          .line-bottom {
+            transform: translateX(2px) rotate(-44deg);
+            transition: transform 0.3s ease-out;
+          }
+        }
+      }
+
+      svg {
+        .line-top {
+          transform-box: fill-box;
+          transform: translateX(0) rotate(0deg);
+          transition: transform 0.3s ease-out;
+        }
+        .line-middle {
+          opacity: 1;
+          transition: opacity 0.2s ease-in;
+        }
+        .line-bottom {
+          transform-box: fill-box;
+          transform: translateX(0) rotate(0deg);
+          transition: transform 0.3s ease-out;
+        }
+      }
+    }
+  }
+
+  .header-panel {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background: $blue1;
+    padding: 40px 20px 60px;
+    position: fixed;
+    top: 60px;
+    left: 0;
+    right: 0;
+    height: calc(100vh - 60px);
+    transform: translateX(100%);
+
+    transition: transform 0.3s ease-out;
+
+    &.-show {
+      transform: translateX(0);
+    }
+
+    .nav-list {
+      width: 100%;
+
+      .nav-item {
+        display: block;
+        width: fit-content;
+
+        + .nav-item {
+          margin-top: 30px;
+        }
+      }
+    }
+
+    .ui-icon {
+      align-self: center;
+      width: 60%;
+      padding-bottom: 100px;
+    }
+  }
+}
+</style>
